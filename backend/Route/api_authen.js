b04584ce6e372b7fd9282e601225f258f05053bd
Jerasin <choi.jerasin@gmail.com>
2021-01-25 20:41:01 +0700

    Sqlite3

--- backend/Route/api_authen.js
+++ backend/Route/api_authen.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const user = require("./../models/user");
 
 router.post("/login", (req, res) => {
   res.json({ result: "login" });
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  user.create();
-  res.json({ result: username, password });
+  await user.create(req.body);
+
+  res.json({ result: "ok", username, password });
 });
 
 module.exports = router;
